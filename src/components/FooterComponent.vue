9f8f7cbb72095dcf8168692012b222c0acb6ddcf
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-22 17:53:55 +0100

    quasi finito footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <footer class="bg-buttercup">
         <div class="flexcontainer">
-            <div class="space">
-                <ul>
+            <div class="space ">
+                <ul class="no-list">
                     <li>
                         <h3>About</h3>
                         <br>
@@ -16,36 +16,58 @@
                     </li>
                 </ul>
             </div>
-            <ul>
-                <li>
-                    <h4>Contact</h4>
-                    <br>
-                    <p>USA,California 20,First Avenue, <br>
-                        California<br>
-                        Tel.:+1 212 485 300 32<br>
-                        Fax:+1212 3752414</p>
-                    info@masterstudy.com
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <h4 class="text-center" id="pages">Pages</h4>
-                    <br>
-                    <ul>
-                        <li>Blog</li>
-                        <li>Home</li>
-                        <li>Shortcodes</li>
-                    </ul>
-                </li>
+            <div class="space">
+                <ul class="no-list">
+                    <li>
+                        <h4>Contact</h4>
+                        <br>
+                        <p>USA,California 20,First Avenue, <br>
+                            California<br>
+                            Tel.:+1 212 485 300 32<br>
+                            Fax:+1212 3752414</p>
+                        info@masterstudy.com
+                    </li>
+                </ul>
+            </div>
+            <div class="space ">
+                <ul class="no-list">
+                    <li>
+                        <h4>Pages</h4>
+                        <br>
+                        <ul>
+                            <div class="d-flex">
+                                <div>
+                                    <li>Blog</li><br>
+                                    <li>Home</li><br>
+                                    <li>Shortcodes</li>
+                                </div>
+                                <div class="ms-5">
+                                    <li>Courses</li><br>
+                                    <li>Membership</li><br>
+                                    <li>Typography</li>
+                                </div>
+                            </div>
+                        </ul>
+                    </li>
 
-            </ul>
-            <ul>
-                <li>
-                    <h4>Blog</h4>
-                    <br>
-                </li>
-
-            </ul>
+                </ul>
+            </div>
+            <div class="space">
+                <h4>Blog</h4>
+                <br>
+                <div class="d-flex flex-column">
+                    <div>
+                        <img src="public/images/photo-1517520287167-4bbf64a00d66-50x50.jpeg" alt="">
+                        <span>Our main target is to "Developing Yourself as a Leader"</span>
+                        <span>-August 9,2018</span>
+                    </div>
+                    <div>
+                        <img src="public/images/photo-1490376840453-5f616fbebe5b-50x50.jpeg" alt="">
+                        <span> Those Other Collage Expenses You Aren't Thinking About</span>
+                        <span>-june 3,2015</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
@@ -69,13 +91,8 @@ footer {
 
 }
 
-ul,
-li {
+.no-list {
     list-style: none;
-}
-
-a {
-    text-decoration: none;
 }
 
 .flexcontainer {
@@ -85,9 +102,5 @@ a {
     padding-top: 30px;
 }
 
-#pages {
-    a {
-        text-decoration: solid;
-    }
-}
+#pages {}
 </style>
